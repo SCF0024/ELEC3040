7ba24f130b4f8ead3b3024df2181556593aadae6
SCF0024 <60124903+SCF0024@users.noreply.github.com>
2020-01-20 22:32:06 -0600

    Add files via upload

---
+++ Lab2.c
@@ -0,0 +1,82 @@
+/*====================================================*/
+/* Stephen Fortner and Tivon */
+/* ELEC 3040/3050 - Lab 2, Program 1 */
+/* Cuount up/down depending on specified direction */
+/*====================================================*/
+
+
+#include "STM32L1xx.h" 						//Microcontroller information
+
+/* Define global variables */
+unsigned char currentCount; 			//number currently displayed
+
+/*---------------------------------------------------*/
+/* Initialize GPIO pins used in the program */
+/* PA1 = start/stop PA2 = direction */
+/* PC[0,3] = output */
+/*---------------------------------------------------*/
+
+void PinSetup () {
+ /* Configure PA1 and PA2 as input pin to read push button */
+ RCC->AHBENR |= 0x01; 						//Enable GPIOA clock (bit 0)
+ GPIOA->MODER &= ~(0x0000003c); 	//General purpose input mode
+	
+ /* Configure PC[0,3] as output pins to drive LEDs */
+ RCC->AHBENR |= 0x04; 						//Enable GPIOC clock (bit 2)
+ GPIOC->MODER &= ~(0x000000FF); 	//Clear PC[0,3] mode bits
+ GPIOC->MODER |= (0x00000055); 		//General purpose output mode
+}
+/*----------------------------------------------------------*/
+/* Delay function - do nothing for about .5 second */
+/*----------------------------------------------------------*/
+
+void delay () {
+ int i,j,n;
+ for (i=0; i<10; i++) { 		//outer loop
+	for (j=0; j<20000; j++) { //inner loop
+		n = j; 									//dummy operation for single-step test
+	} 												//do nothing
+ }
+}
+
+/*------------------------------------------------*/
+/* Count function - increment or decrement according to direction*/
+/*------------------------------------------------*/
+
+void count (unsigned char direction) {
+	//if direction = 1 count down or if currently 0 go to 9
+	if (direction == 1){
+		if (currentCount == 0){
+			currentCount = (currentCount + 1) % 10;
+		}
+		else {
+			currentCount = (currentCount +(10 - 1)) % 10;
+		}
+	}
+	
+	//display count information
+	GPIOC ->BSRR |= (~currentCount & 0x0F) << 16; //clear bits
+	GPIOC ->BSRR |= (currentCount & 0x0F);				//write bits
+}
+
+/*------------------------------------------------*/
+/* Main program */
+/*------------------------------------------------*/
+
+int main(void) {
+	unsigned char sw1 = 0; 					//start/stop
+	unsigned char sw2 = 0;					//direction
+
+	PinSetup();					  			//Configure GPIO pins
+	
+	/* Endless loop */
+	while (1) {
+		sw1 = GPIOA->IDR & 0x02; 	//Read GPIOA and mask all but bit 1
+		sw2 = GPIOA->IDR & 0x04;	//Read GPIOA and mask all but bit 2
+		if (sw1){
+			count(sw2);
+		}
+		delay(); 									//Time delay for counting
+		
+ }														/* repeat forever */
+}
